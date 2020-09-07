fd6403762aca905062d5c0e823dedc61ae5bdcc2
ojasvi jain <wk68qMGGiqt57M9>
2020-09-07 18:08:12 +0600

    Added role_info command

--- cogs/Helpful.py
+++ cogs/Helpful.py
@@ -11,6 +11,19 @@ class Helpful(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def role_info(self, ctx, role: discord.Role) -> None:
+        """this fuction returns the names of all the member with that role."""
+        embed = discord.Embed(
+            title=f"List of people with the role {role.name}:[Total members with the role:[{len(role.members)}]"
+            , color=role.color)
+
+        for member in role.members:
+            embed.add_field(name="~~~~~~~~~~~~~~~~~"
+                            , value=f"{member.name}")
+
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Helpful(bot))
