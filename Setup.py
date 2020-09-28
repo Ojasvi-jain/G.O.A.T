51c62ce0a71e8416cd306103a1d051568fce8f4b
ojasvi jain <ojasvi.jaain@gmail.com>
2020-09-28 21:17:25 +0600

    Optimised loading extensions

--- Setup.py
+++ Setup.py
@@ -7,15 +7,16 @@ from datetime import datetime
 import asyncio
 import os
 
-client = commands.Bot(command_prefix=">")
+client = commands.Bot(command_prefix=">", case_insensitive=True)
 token = os.getenv('DISCORD_TOKEN')
 client.remove_command('help')
 
+
 def start():
+    cogs = ["cogs.Events", "cogs.Helpful", "cogs.Mod_Commands", "cogs.Fun"]
     print("Initialize the cogs loading process")
-    client.load_extension("cogs.Fun")
-    client.load_extension("cogs.Mod_Commands")
-    client.load_extension("cogs.Helpful")
+    for i in cogs:
+        client.load_extension(i)
     print("Cogs loaded successfully")
     client.run(token)
 
