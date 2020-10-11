cc22c14538f2e2c9de5c08b6c12b2429142cfd03
Ojasvi-jain <ojasvi.jaain@gmail.com>
2020-10-11 17:58:18 +0600

    Reverted back to good commit

--- Setup.py
+++ Setup.py
@@ -1,12 +1,7 @@
 import os
-import discord
 from discord.ext import commands
 
-client = commands.Bot(
-    command_prefix=">",
-    case_insensitive=True,
-    intents=discord.Intents.all()
-)
+client = commands.Bot(command_prefix=">", case_insensitive=True)
 token = os.environ['DISCORD_TOKEN']
 client.remove_command('help')
 
@@ -16,10 +11,7 @@ def start():
     print("Initialize the cogs loading process")
     for i in cogs:
         client.load_extension(i)
-    print("Cogs loaded successfully \n")
-    print('Active in these guilds/servers:')
-    [print(g.name) for g in client.guilds]
-    print('\n G.O.A.T started successfully', end="\n ------------------------------- \n")
+    print("Cogs loaded successfully")
     client.run(token)
 
 
--- cogs/Events.py
+++ cogs/Events.py
@@ -36,32 +36,6 @@ class events(commands.Cog):
 
             await message.channel.send(embed=embed)
 
-    @commands.Cog.listener()
-    async def on_guild_join(self, guild):
-        if constants.Channels.new_guild_channel:
-            users = sum(1 for m in guild.members if not m.bot)
-            bots = sum(1 for m in guild.members if m.bot)
-            members = f"Humans: `{users}/{len(guild.members)}` \n Bots: `{bots}/{len(guild.members)}`"
-            embed = discord.Embed(name=f"{self.bot.user.name} has joined a guild")
-            embed.set_footer(
-                text=f"Guild: {len(self.bot.guilds):,} | Shard: {guild.shard_id}/{self.bot.shard_count - 1} | rejoin"
-            )
-            guild_text = (
-                f"Name: `{guild.name}`\n"
-                f"ID: `{guild.id}`\n"
-                f"Owner ID: `{guild.owner.id}`\n"
-            )
-
-            embed.add_field(name="Guild", value=guild_text)
-            embed.add_field(name="Region", value=guild.region)
-            embed.timestamp = datetime.now()
-            if guild.icon_url:
-                embed.set_thumbnail(url=guild.icon_url)
-            else:
-                embed.set_thumbnail(url="https://i.imgur.com/AFABgjD.png")
-            channel = self.bot.get_channel(constants.Channels.new_guild_channel)
-            await channel.send(embed=embed)
-
 
 def setup(bot):
     bot.add_cog(events(bot))
