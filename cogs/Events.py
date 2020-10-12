8039824b57166e591135fec0f6a26436535f3d3e
Ojasvi-jain <ojasvi.jaain@gmail.com>
2020-10-12 10:49:11 +0600

    Added on_member_join command

--- cogs/Events.py
+++ cogs/Events.py
@@ -36,6 +36,11 @@ class events(commands.Cog):
 
             await message.channel.send(embed=embed)
 
+    async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        if channel is not None:
+            await channel.send(f"Welcome {member}")
+
 
 def setup(bot):
     bot.add_cog(events(bot))
